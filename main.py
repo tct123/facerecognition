9ddd1525c0226e6e71bae86c89245cee941c0d14
tct123 <42028373+tct123@users.noreply.github.com>
2023-08-25 06:18:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import cv2
+
 capture = cv2.VideoCapture(0)
 cascade = cv2.CascadeClassifier(filename="haarcascade_frontalface_default.xml")
 while True:
-    _,im=capture.read()
-    im_grey = cv2.cvtColor(im,code=cv2.COLOR_BGR2GRAY)
+    _, im = capture.read()
+    im_grey = cv2.cvtColor(im, code=cv2.COLOR_BGR2GRAY)
     faces = cascade.detectMultiScale(image=im_grey)
-    for x,y,width,height in faces:
-        cv2.rectangle(im, (x,y), (x + width, y + height), color=(255, 0, 0), thickness=5)
+    for x, y, width, height in faces:
+        cv2.rectangle(
+            im, (x, y), (x + width, y + height), color=(255, 0, 0), thickness=5
+        )
     cv2.imshow("Camera", im)
     if cv2.waitKey(1) == ord("q"):
         break
